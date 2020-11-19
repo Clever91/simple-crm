387b05feb96225c1a5c64ce6d098ce9686524f86
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2020-11-19 18:53:29 +0500

    added all view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+import AuthLayout from '@/layout/AuthLayout'
+import MainLayout from '@/layout/MainLayout'
+export default {
+  computed: {
+    layout () {
+      console.log(this.$route.meta)
+      return (this.$route.meta.layout || 'auth') + '-layout'
     }
+  },
+  components: {
+    AuthLayout, MainLayout
   }
 }
+</script>
+
+<style lang="scss">
+@import 'assets/css/index.css';
+@import '~materialize-css/dist/css/materialize.min.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,71 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
+// view
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Category from '../views/Categories.vue'
+import DetailRecord from '../views/DetailRecord.vue'
+import History from '../views/Histories.vue'
+import Planning from '../views/Planning.vue'
+import Profile from '../views/Profile.vue'
+import Record from '../views/Record.vue'
+import Register from '../views/Register.vue'
+
+// route
 const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { layout: 'main' },
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'auth' },
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'auth' },
+    component: Register
+  },
+  {
+    path: '/categories',
+    name: 'category',
+    meta: { layout: 'main' },
+    component: Category
+  },
+  {
+    path: '/detail/record',
+    name: 'detail-record',
+    meta: { layout: 'main' },
+    component: DetailRecord
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: History
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: Planning
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: Profile
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: Record
   }
 ]
 
