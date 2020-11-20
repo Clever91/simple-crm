d8a3e9c2189c95244011c1cef1d6f54cf68abb78
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2020-11-20 17:29:57 +0500

    uxxx

---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="sidenav app-sidenav" :class="{open: value}">
+        <li v-for="link in links" :key="link.url" :class="{active: link.exact}">
+            <router-link :to="link.url" class="waves-effect waves-orange pointer">{{ link.title }}</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  props: ['value'],
+  data: () => ({
+    links: [
+      { title: 'Счет', url: '/', exact: true },
+      { title: 'История', url: '/history' },
+      { title: 'Планирование', url: '/planning' },
+      { title: 'Новая запись', url: '/record' },
+      { title: 'Категории', url: '/categories' }
+    ]
+  })
+}
+</script>
